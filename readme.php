62a36ebf9217e1c415cde5ae472c894530970cad
Sandeep Mehra <smehra124@gmail.com>
2025-07-27 13:37:27 +0530

    Create readme.php

---
+++ readme.php
@@ -0,0 +1,8 @@
+Date 27 July 2025 | v4.6.0
+
+
+composer create-project codeigniter4/appstarter aaa
+php spark phpini:check
+php spark cache:clear Or simply delete the writable/cache/FileLocatorCache file.
+
+Config\Services.php usefull || user_guide/concepts/services.html
